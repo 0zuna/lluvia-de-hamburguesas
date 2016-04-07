7e618dac587e76d7ca4a671807b88c66be0e5bcd
Ozuna <er1k_92@hotmail.com>
2016-04-07 13:02:37 -0500

    lluvia de hamburguesas

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+window.onload = function() {
+
+        var game = new Phaser.Game(886, 547, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+	game.load.image('lluvia', 'http://peliculas.chiquipedia.com/images/peliculas-infantiles-lluvia-hamburguesas-2.jpg?phpMyAdmin=9ea091c51a5aa3cf876fb3cf0a5f7f3d');
+	game.load.image('ground', 'assets/platform.png');
+	game.load.image('star', 'assets/star.png');
+	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
+
+function create() {
+	game.add.sprite(0, 0, 'lluvia');
+}
+
+function update() {
+}
+
+    };
