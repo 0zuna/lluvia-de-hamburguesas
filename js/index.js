bbd624602481fe16484c84c5bc0e6db85a902103
0zuna <er1k_92@hotmail.com>
2016-04-07 18:44:01 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,39 @@
 window.onload = function() {
-	var game = new Phaser.Game(886, 547, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+	var game = new Phaser.Game("100", "100", Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 	function preload() {
-		game.load.image('lluvia', 'http://peliculas.chiquipedia.com/images/peliculas-infantiles-lluvia-hamburguesas-2.jpg?phpMyAdmin=9ea091c51a5aa3cf876fb3cf0a5f7f3d');
-		game.load.image('ground', 'assets/platform.png');
-		game.load.image('star', 'assets/star.png');
-		game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-		game.scale.pageAlignVertically = false;
-        game.scale.pageAlignHorizontally = false;
+		game.load.image('lluvia', 'assets/cloudy-with-a-chance-of-meatballss.jpg');
+		game.load.spritesheet('flint', 'assets/flint.png', 52, 121);
+		game.load.image('hamburguesa','assets/hamburguesa.png')
+		//game.scale.pageAlignVertically = true;
+        //game.scale.pageAlignHorizontally = true;
+        game.debug.text('Lluvia de Hamburguesas', 270, 16);
+        console.log('preload');
+        
+
 	}
 
 	function create() {
-		game.add.sprite(0, 0, 'lluvia');
 		
+		lluvia=game.add.sprite(0, 0, 'lluvia');
+		game.add.sprite(50,50,'flint');
+		lluvia.height = game.height;
+    	lluvia.width = game.width;
+    	lluvia.smoothed = true;
+		console.log('create')
+
+    	hamburguesas = game.add.group();
+    	hamburguesas.enableBody = true;
+
+    	while (hamburguesas){
+    	//var i=1;
+        var burguer = hamburguesas.create(i * 70, 0, 'hamburguesa');
+        burguer.body.gravity.y = Math.floor((Math.random() * 100) + 1); 
+        burguer.body.bounce.y = 0.7 + Math.random() * 0.2;
+        //i++;
+    	}
+
+       
 	}
 
 	function update() {
