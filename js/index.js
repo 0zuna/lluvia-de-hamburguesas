ce4e95abfc1b2d8d2286308377ceb9229fcaa43c
Ozuna <er1k_92@hotmail.com>
2016-04-12 20:53:25 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var game = new Phaser.Game("100", "100", Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
+	var platforms;
 	function preload() {
 		game.load.image('lluvia', 'assets/cloudy-with-a-chance-of-meatballss.jpg');
 		game.load.spritesheet('flint', 'assets/flint.png', 52, 121);
@@ -25,13 +25,23 @@ window.onload = function() {
     	hamburguesas = game.add.group();
     	hamburguesas.enableBody = true;
 
-    	while (hamburguesas){
-    	//var i=1;
-        var burguer = hamburguesas.create(i * 70, 0, 'hamburguesa');
+    	
+    	for (var i=1;i<12;i++){
+        var burguer = hamburguesas.create(i*Math.floor((Math.random() * 100) + 1) , 0, 'hamburguesa');
         burguer.body.gravity.y = Math.floor((Math.random() * 100) + 1); 
         burguer.body.bounce.y = 0.7 + Math.random() * 0.2;
-        //i++;
     	}
+        platforms = game.add.group();
+        
+	ground = platforms.create(0, game.world.height - 64, '');
+	ground.enableBody = true;
+    ground.scale.setTo(2, 2);
+   	//ground.body.immovable = true;
+    //var ledge = platforms.create(400, 400, 'lluvia');
+    //ledge.body.immovable = true;
+    //ledge = platforms.create(-150, 250, 'lluvia');
+    //ledge.body.immovable = true;
+    	
 
        
 	}
